8a4bcb9f29395a34e70111de7ce25d1a3ae585c5
Ignacio Chiaravalle <nachochiara@gmail.com>
2020-06-14 00:27:19 -0300

    Created welcome page, but submition is not ready.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,24 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+	<head>
+		<title>The Water Level - Login</title>
+		<script src = "{{ url('/js/CheckUser.js') }}" type = "text/javascript"></script>
+		<link rel = "stylesheet" type = "text/css" href = "{{ url('/css/Document Style.css') }}"/>
+		<link rel = "stylesheet" type = "text/css" href = "{{ url('/css/Welcome Style.css') }}"/>
+	</head>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+	<body>
+		<h1>
+			¡¡Bienvenido al Administrador de Stock de<br>
+			The Water Level!!
+		</h1>
+        <form method="POST" action="{{ url('redirect.blade.php') }}">
+			<label id="description">Por favor, ingrese sus datos de usuario para continuar:</label><br>
+			<label for="username">Nombre de Usuario:</label><br>
+			<input type="text" id="username" class="default-field" value="Usuario" onselect = "clearFieldIfDefault(this)" onclick="clearFieldIfDefault(this)"><br>
+			<label for="password">Contraseña:</label><br>
+			<input type="text" id="password" class="default-field" value="Contraseña" onselect = "clearFieldIfDefault(this); swapType(this)" onclick="clearFieldIfDefault(this); swapType(this)"><br><br>
+			<input type="submit" class="submit disabled-submit" id="submit-btn" value="Confirmar" disabled="disabled">
+		</form>
+	</body>
 </html>
