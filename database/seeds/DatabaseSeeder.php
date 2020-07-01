fe02b8e663e1ae55ef3b1b73f6830ef639da0c05
Ignacio Chiaravalle <nachochiara@gmail.com>
2020-07-01 19:42:34 -0300

    Removed comment on DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,10 +23,8 @@ class DatabaseSeeder extends Seeder {
 	}
 
 	private function truncateTables(array $tables) {
-		//DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
 		foreach ($tables as $table)
 			DB::table($table)->truncate();
-		//DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
 	}
 
 }
